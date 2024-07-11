6cd99003bae412f00eae90f5f0ef7b715044d1c4
jgp-shishuaige <9005413+the-handsome-guy-is-me@user.noreply.gitee.com>
2024-07-11 16:58:15 +0800

    'mybaits自动生成代码'

---
+++ rookie-im-server/src/main/java/com/rookie/im/user/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.rookie.im.user.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 菜鸟 IM 用户表 前端控制器
+ * </p>
+ *
+ * @author paoge
+ * @since 2024-07-11
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
+
---
+++ rookie-im-server/src/main/java/com/rookie/im/user/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.rookie.im.user.service;
+
+import com.rookie.im.user.domain.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 菜鸟 IM 用户表 服务类
+ * </p>
+ *
+ * @author paoge
+ * @since 2024-07-11
+ */
+public interface IUserService  {
+
+}
